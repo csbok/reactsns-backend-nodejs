3d8bfe6f07c8116033b12facca61f2ee6918fa8e
csbok <winmain@gmail.com>
2015-11-18 17:17:04 +0900

    nested query - article, comment

--- controllers/article.js
+++ controllers/article.js
@@ -34,12 +34,17 @@ articleController.timeline = function(req, res) {
 			if (err) console.error('err : ' + err);
 			console.log('rows : ' + JSON.stringify(rows));
 
-	//			res.render('index', {title: 'test', rows: rows});
-			connection.release();
+		    var pending = rows.length;
 
-			res.send(rows);
-
-			// Don't use the connection here, it has been returned to the pool.
+		 	rows.forEach(function (row) {
+		        connection.query('SELECT comment_no, comment.comment, user.user_no, user.display_name FROM comment,user WHERE comment.user_no = user.user_no AND article_no = ?',  row.article_no , function(err, comment){
+		        	row.comment_list = comment;
+		            if( 0 === --pending ) {
+						connection.release();
+						res.send(rows);
+		            }
+		        });
+		 	});
 		});
 	});
 
@@ -56,18 +61,35 @@ articleController.newArticle = function(req,res) {
 				if (err) console.error('err : ' + err);
 				console.log('rows : ' + JSON.stringify(rows));
 
-				conn.release();
+			    var pending = rows.length;
+ 
+			 	rows.forEach(function (row) {
+			        conn.query('SELECT comment_no, comment.comment, user.user_no, user.display_name FROM comment,user WHERE comment.user_no = user.user_no AND article_no = ?',  row.article_no , function(err, comment){
+			        	row.comment_list = comment;
+			            if( 0 === --pending ) {
+							conn.release();
+							res.send(rows);
+			            }
+			        });
+			 	});
 
-				res.send(rows);
 			});
 		} else {
 			conn.query('select article_no, content, article.user_no, (select count(1) from good where good_article_no=article_no) as good_count, user_name as author from article, user where article.user_no = user.user_no ORDER BY article_no DESC', function(err, rows) {
 				if (err) console.error('err : ' + err);
-				console.log('rows : ' + JSON.stringify(rows));
 
-				conn.release();
+			    var pending = rows.length;
+ 
+			 	rows.forEach(function (row) {
+			        conn.query('SELECT comment_no, comment.comment, user.user_no, user.display_name FROM comment,user WHERE comment.user_no = user.user_no AND article_no = ?',  row.article_no , function(err, comment){
+			        	row.comment_list = comment;
+			            if( 0 === --pending ) {
+							conn.release();
+							res.send(rows);
+			            }
+			        });
+			 	});
 
-				res.send(rows);
 			});
 		}
 	});
